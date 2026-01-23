86b920ebf0f86fe8fd241903df8a89bafbce5f14
unmoved21 <unmoved2121@gmail.com>
2026-01-23 00:57:19 +0000

    onyx: Include firmware

--- Android.mk
+++ Android.mk
@@ -3,3 +3,38 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),onyx)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,57375ab5e9abe524ad28d801a041e8c6f3c27164)
+$(call add-radio-file-sha1-checked,radio/aop.img,f1fd74613c202c44c94d296240d60a8cea8db6e7)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,db8dbc5fda9199f117056010d9ae2d6269131427)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,b27b0c9b6a70f6d4be376f2b7a86e4bcfb5d524c)
+$(call add-radio-file-sha1-checked,radio/countrycode.img,e9b664ebcbe623aeac43b70910461b138fbed383)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,f4ddd04a7bbe4cb29e0a09de98f46647f1a1da72)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,f5ff3a2f6f1ca5b677afcd8a0246555b14d9181f)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,1665d2270146c41bb5ba21bd13b61abf7c377200)
+$(call add-radio-file-sha1-checked,radio/dsp.img,eb9b99242d329ca1e2815a6ba14cb48805c93761)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,a7b369166a3792bce410af72860c8769a31ce822)
+$(call add-radio-file-sha1-checked,radio/hyp.img,2c9dae273cccc7e0e0d1f8aa78e08e07bd2213aa)
+$(call add-radio-file-sha1-checked,radio/idmanager.img,1f867365d1ef66ec0a2219dff362db9411b417d0)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,1fb11689a83178be8e8f7bc888a6b770f8df9aa5)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,184cac3f2b53698918418be4dd10b2d52164727b)
+$(call add-radio-file-sha1-checked,radio/modem.img,cb7260544869d0f4059b078aaf6dbbf920c078c1)
+$(call add-radio-file-sha1-checked,radio/modemfirmware.img,48725e6bae876fb20b7d9d1f0a97b4d9b2795d3f)
+$(call add-radio-file-sha1-checked,radio/multiimgqti.img,2ea8427aaf9ed6e9d171edc2e9f2607815cd1583)
+$(call add-radio-file-sha1-checked,radio/pvmfw.img,f9784ca92dd43a985bb03a946a8933bc0d9c6506)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,6c3b0983176d2b9c88500dcf9a9b28509d44d409)
+$(call add-radio-file-sha1-checked,radio/shrm.img,5e603df14254617064b9074cfa234eb7900bb79b)
+$(call add-radio-file-sha1-checked,radio/soccp_dcd.img,941eb85c5b7e4d59ef5a69e8756193108befeca9)
+$(call add-radio-file-sha1-checked,radio/soccp_debug.img,e3387a348b51f4a4872613a877ea2bbde65ec3f6)
+$(call add-radio-file-sha1-checked,radio/spuservice.img,906f826e99252c3648418fddb76dc0ee145fa1f3)
+$(call add-radio-file-sha1-checked,radio/tz.img,25e01151720a119282370e1350670bf6a300c857)
+$(call add-radio-file-sha1-checked,radio/uefi.img,9593bf4e6e84765475c8dffe7b72575fcebb5dc5)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,62c26dc4f0c4c33c75edcab2008004fa937138b3)
+$(call add-radio-file-sha1-checked,radio/vm-bootsys.img,069778173021ad699317b979775ed0398d95f704)
+$(call add-radio-file-sha1-checked,radio/xbl.img,0cf74287ff28201109e6918371959a6b54947e86)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,45e9128fea1843079cf31457ace56f3d6ee6f074)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,8e4a4cfb0197b14c5a56946ba0d900faf7dc0dfd)
+
+endif
