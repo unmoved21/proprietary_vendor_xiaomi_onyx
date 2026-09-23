ba0c216a1f2036da555a0bd3b5ed17e519b83e9d
unmoved21 <unmoved2121@gmail.com>
2026-09-23 22:53:24 +0000

    Revert "onyx: Bring back prebuilt soundfx blobs"
    This reverts commit 5ce7d2b9b2391e3bbe61339abc249e1f9337448d.

--- onyx-vendor.mk
+++ onyx-vendor.mk
@@ -2803,17 +2803,7 @@ PRODUCT_PACKAGES += \
     libvpt_action_recognition \
     libworker_pool \
     sensors.qsh \
-    libbundleaidl \
-    libdownmixaidl \
-    libdynamicsprocessingaidl \
-    libloudnessenhanceraidl \
-    libqcompostprocbundle \
-    libqcomvisualizer \
-    libqcomvoiceprocessing \
     libquasar \
-    libreverbaidl \
-    libvisualizeraidl \
-    libvolumelistener \
     vendor.display.color@1.0 \
     vendor.display.color@1.1 \
     vendor.display.color@1.2 \
